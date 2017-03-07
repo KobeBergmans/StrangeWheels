d7b13a815cd591702cdf94f6e386ab28ee119295
Marcus Webb <marcusdavidwebb@gmail.com>
2017-03-07 17:33:32 +0100

    Road2Wheel works, plotting almost

--- src/Wheel2Road.jl
+++ src/Wheel2Road.jl
@@ -1,17 +1,5 @@
 
-# Create a function for the wheel in polar coords
-# r = Θ -> 1 + .25*abs(sin(3*Θ));                % Flower
-# r = Θ -> 1 + .25*(Θ-pi/2).^2;                % Acorn
-# r = Θ -> max(1 + sign(Θ),0.05);              % Pacman
-# r = Θ -> 1 + .5*sin(2*(Θ));             % Peanut
-# r = Θ -> 1+ sin(Θ-pi/2);                     % Cardioid
-# r = Θ -> 1./(abs(cos(Θ))+abs(sin(Θ)));   % Square
-N = 5;                                                # N-sided polygon
- r = Θ -> -csc(restrict(Θ,-pi/2-pi/N,-pi/2+pi/N));
-# r = Θ -> 1 + abs(Θ);                         % Crazy snail-shell
-
-
-function wheel2road(r,h=0.0005)
+function wheel2road(r=Θ -> 1 + .25*abs(sin(3*Θ)),h=0.0005)
   # Input:
   # Function of wheel in polar coordinates, step-size h
   # Output:
@@ -35,30 +23,49 @@ function wheel2road(r,h=0.0005)
   Θ,x,y
 end
 
+function plotwheel(t,Θ,x,y,r)
+  # Input: time t which is between 1 and 100
 
-function wheel2interact(r)
-
-  Θ,x,y = wheel2road(r,h)
-
-  # Produce the points traced out by the wheel for all times
-  n = length(x)
+  k = t*div(length(x),100)
   angles = linspace(-pi/2,3*pi/2,1000)
-  angles = [angles,angles(1)]  # Ensures the wheel is a closed curve
-  anglemesh = ones(n,1)*angles - (pi/2 + Θ)*ones(1,1001)
-  radiusmesh = ones(n,1)*r(angles)
-  wheelx = x*ones(1,1001) + radiusmesh.*cos(anglemesh)
-  wheely = radiusmesh.*sin(anglemesh)
+  angles = [angles;angles[1]]  # Ensures the wheel is a closed curve
+  rads = r(angles)
+  M = maximum(rads)
+  angles = angles - pi/2 +Θ[k]
 
-  M = maximum(r(angles))
-
-  # Preallocate number of frames
-  nframes = 100;
-  step = floor(n/nframes);
-
-  #@manipulate for k = 1:step:n
-    plot(x,y,linewidth=3,xlims=(0,3pi*M),ylims=(-2M,2M))   # Road
-    plot!([0,3pi*M],[0,0],color=:black,linestyle=:dot)    # Level of axle
-    plot!(x[k]+M*.05*cos(angles),M*.05*sin(angles),color=:black,linewidth=3) # axle
-    plot!(wheelx[k,:],wheely[k,:],color=:black,linewidth=3) # wheel at time t_k
-  #end
+  # Road
+  plot(x,y,linewidth=3,xlims=(0,3pi*M),ylims=(-2.5M,2.5M),legend=false)
+  # Level of axle
+  plot!([0,3pi*M],[0,0],color=:black,linestyle=:dash)
+  # axle
+  plot!(x[k]+M*.05*cos(angles),M*.05*sin(angles),color=:black,linewidth=3)
+  # wheel at time t_k
+  plot!(x[k]+rads.*sin(angles),rads.*cos(angles),color=:black,linewidth=3)
 end
+
+# function wheel2interact(r,h=0.0005)
+#
+#   Θ,x,y = wheel2road(r,h)
+#
+#   # Produce the points traced out by the wheel for all times
+#   n = length(x)
+#   angles = linspace(-pi/2,3*pi/2,1000)
+#   angles = [angles;angles[1]]  # Ensures the wheel is a closed curve
+#   anglemesh = ones(n)*angles' - (pi/2 + Θ)*ones(1001)'
+#   radiusmesh = ones(n)*r(angles)'
+#   wheelx = x*ones(1001)' + radiusmesh.*cos(anglemesh)
+#   wheely = radiusmesh.*sin(anglemesh)
+#
+#   M = maximum(r(angles))
+#
+#   # Preallocate number of frames
+#   nframes = 100;
+#   step = floor(n/nframes);
+#
+#   @manipulate for k = 1:step:n
+#     plot(x,y,linewidth=3,xlims=(0,3pi*M),ylims=(-2M,2M),legend=false)   # Road
+#     plot!([0,3pi*M],[0,0],color=:black,linestyle=:dot)    # Level of axle
+#     plot!(x[k]+M*.05*cos(angles),M*.05*sin(angles),color=:black,linewidth=3) # axle
+#     plot!(wheelx[k,:],wheely[k,:],color=:black,linewidth=3) # wheel at time t_k
+#   end
+# end
