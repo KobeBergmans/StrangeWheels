a22928da80317b82454972794197d54eb66e06d0
Marcus Webb <marcusdavidwebb@gmail.com>
2017-03-13 09:59:59 +0100

    wheel2road and road2wheel simplified. Restructered files

--- src/Wheel2Road.jl
+++ src/Wheel2Road.jl
@@ -1,39 +1,36 @@
 
-function wheel2road(r=Θ -> 1 + .25*abs(sin(3*Θ)),dΘ=0.002π)
-  # Input:
-  # Function of wheel in polar coordinates, step-size dΘ
-  # Output:
-  # Θ: vector of 1500  dΘ-equispaced angles, Θ[1] = -π/2, 1.5 turns
-  # x: vector of 1500 positions of the wheel axle
-  # y: vector of 1500 heights of the road
+function wheel2road(r = Θ->1+.25abs(sin(3Θ)))
+  # Input: Function of wheel in polar coordinates
+  # Output: 1 revolution of the wheel on road
+  # Θ: linspace of 1000 equispaced angles, Θ[1] = -π/2
+  # x: vector of 1000 positions of the wheel axle
+  # y: vector of 1000 heights of the road
 
-  Θ = linspace(-π/2,2.5π,1500)
-  x = cumsum(r(Θ)*dΘ)
+  Θ = linspace(-π/2,3π/2,1000)
+  dΘ = 0.002π
+  x = cumsum(r(Θ)dΘ)          # Euler's method for x'(Θ) = r(Θ)
   y = -r(Θ)
+  y[end] = y[1]
 
   Θ,x,y
 end
 
-#function road2wheel(y = x->1)
+function road2wheel(f = x->1)
+  # Input: Function for road, f(x)
+  # Output: 1 revolution of the wheel on road
+  # Θ: linspace of 1000 equispaced angles, Θ[1] = -π/2
+  # x: vector of 1000 positions of the wheel axle
+  # y: vector of 1000 heights of the road
 
-function plotwheel(t,Θ,x,y,r)
-  # Input: time t which is between 1 and 100
+  Θ = linspace(-π/2,3π/2,1000)
+  dΘ = 0.002π
+  x = zeros(1000)
+  y = zeros(1000)
+  for k = 2:1000
+    y[k-1] = yofx(x[k-1])
+    x[k] = x[k-1] - h*f(x[k-1])   # Euler's method for x'(Θ) = -f(x(Θ))
+  end
+  y[end] = y[1]
 
-  # Grid has 1500 pts
-  k = 15t
-
-  # Angle spacing small for sharp edges and first = last + 2π
-  angles = linspace(-π/2,1.5π,1000)
-  rads = r(angles)
-  M = maximum(rads)
-  angles = angles - π/2 - Θ[k]
-
-  # Road
-  plot(x,y,linewidth=3,xlims=(0,3π*M),ylims=(-3M,3M),legend=false)
-  # Level of axle
-  plot!([0,3π*M],[0,0],color=:black,linestyle=:dash)
-  # axle
-  plot!(x[k]+M*.05cos(angles),M*.05sin(angles),color=:black,linewidth=3)
-  # wheel at time t
-  plot!(x[k]+rads.*cos(angles),rads.*sin(angles),color=:black,linewidth=3)
+  Θ,x,y
 end
