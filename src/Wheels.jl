a4ce81ed109de9534c4bdede84c079b1269a4178
Marcus Webb <marcusdavidwebb@gmail.com>
2017-03-13 10:01:06 +0100

    Commented out example functions

--- src/Wheels.jl
+++ src/Wheels.jl
@@ -10,12 +10,12 @@ include("Road2Wheel.jl")
 
 
 # Create a function for the wheel in polar coords
-r1 = Θ -> 1 + .25*abs(sin(3*Θ));              # Flower
-  r2 = Θ -> 1 + .25*(Θ-pi/2).^2;                # Acorn
-  r3 = Θ -> max(1 + sign(Θ),0.05);              # Pacman
-  r4 = Θ -> 1 + .5*sin(2*(Θ));             # Peanut
-  r5 = Θ -> 1+ sin(Θ-pi/2);                     # Cardioid
-  r6 = Θ -> 1./(abs(cos(Θ))+abs(sin(Θ)));   # Square
-  N = 5                                       # N-sided polygon
-  r7 = Θ -> -csc(restrict(Θ,-pi/2-pi/N,-pi/2+pi/N));
-  r8 = Θ -> 1 + abs(Θ);                         # Crazy snail-shell
+#r1 = Θ -> 1 + .25*abs(sin(3*Θ));              # Flower
+#  r2 = Θ -> 1 + .25*(Θ-pi/2).^2;                # Acorn
+#  r3 = Θ -> max(1 + sign(Θ),0.05);              # Pacman
+#  r4 = Θ -> 1 + .5*sin(2*(Θ));             # Peanut
+#  r5 = Θ -> 1+ sin(Θ-pi/2);                     # Cardioid
+#  r6 = Θ -> 1./(abs(cos(Θ))+abs(sin(Θ)));   # Square
+#  N = 5                                       # N-sided polygon
+#  r7 = Θ -> -csc(restrict(Θ,-pi/2-pi/N,-pi/2+pi/N));
+#  r8 = Θ -> 1 + abs(Θ);                         # Crazy snail-shell
